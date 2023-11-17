ab7693f7578fbc94ebfd6c019d4ec488b79c5c5c
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2023-11-17 10:50:27 +0400

    boilerplate

---
+++ web-app/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/db-viewer": "http://localhost:5000",
+    },
+  },
+})
