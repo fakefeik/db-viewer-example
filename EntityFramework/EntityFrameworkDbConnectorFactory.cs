d481aaf7ef9680b772fc3677d61b09cfd2ae0f57
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2023-11-22 12:06:12 +0500

    ef integration

---
+++ EntityFramework/EntityFrameworkDbConnectorFactory.cs
@@ -0,0 +1,19 @@
+﻿using SkbKontur.DbViewer.Connector;
+using SkbKontur.DbViewer.EntityFramework;
+
+namespace DbViewerExample.EntityFramework;
+
+public class EntityFrameworkDbConnectorFactory : IDbConnectorFactory
+{
+    private readonly Func<EntityFrameworkDbContext> _createContext;
+
+    public EntityFrameworkDbConnectorFactory(Func<EntityFrameworkDbContext> createContext)
+    {
+        _createContext = createContext;
+    }
+
+    public IDbConnector CreateConnector<T>() where T : class
+    {
+        return new EntityFrameworkDbConnector<T>(_createContext);
+    }
+}
