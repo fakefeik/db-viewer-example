a482638cb68d4b8890e26ba24bd2f382b9a6637f
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2023-12-02 14:34:59 +0500

    move code to snippets

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 using System.Text.Json.Serialization;
 using DbViewerExample;
-using DbViewerExample.EntityFramework;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IControllerFactory>(new GroboControllerFactory());
 
 builder.Services.AddControllers()
+    .AddNewtonsoftJson()
     .AddJsonOptions(o => o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,18 +1,5 @@
-import {DbViewerApplication, DbViewerApi, NullCustomRenderer} from "@skbkontur/db-viewer-ui";
-import {BrowserRouter} from "react-router-dom";
-
 function App() {
-    return (
-        <BrowserRouter>
-            <DbViewerApplication
-                dbViewerApi={new DbViewerApi("/db-viewer/")}
-                customRenderer={new NullCustomRenderer()}
-                identifierKeywords={[]}
-                useErrorHandlingContainer
-                isSuperUser
-            />
-        </BrowserRouter>
-    )
+    return "Hello, world"
 }
 
 export default App
